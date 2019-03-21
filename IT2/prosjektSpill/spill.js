807d430b8e826351c1bbd97c18dd359b611278c0
Theakvg <thea.kvinnegard@gmail.com>
2019-03-21 14:22:14 +0100

    joj

---
+++ IT2/prosjektSpill/spill.js
@@ -0,0 +1,23 @@
+
+let gameboard = document.querySelector("#gameboard");
+let firkant = document.querySelectorAll("#firkant");
+
+let spillebrikke = [
+    ["Bilde", "farge", "firkant", "grid", "litenSirkel", "brukt"],
+    ["brikke1", true, true, true, true, false],
+    ["brikke2", true, true, true, false, false],
+    ["brikke3", true, true, false, true, false],
+    ["brikke4", true, false, true, true, false],
+    ["brikke5", false, true, true, true, false],
+    ["brikke6", true, true, false, false, false],
+    ["brikke7", true, false, false, true, false],
+    ["brikke8", false, false, true, true, false],
+    ["brikke9", true, false, true, false, false],
+    ["brikke10", false, true, false, true, false],
+    ["brikke11", false, true, true, false, false],
+    ["brikke12", true, false, false, false, false],
+    ["brikke13", false, true, false, false, false],
+    ["brikke14", false, false, true, false, false],
+    ["brikke15", false, false, false, true, false],
+    ["brikke16", false, false, false, false, false]
+]
